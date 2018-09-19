d920ff1d801d2626639dcff6ae39b5ac4f302fc6
Xiaonan Shen <sxn@shenxn.io>
2018-09-19 17:51:58 -0400

    FC-70 [Add] Add VoyagerSeeder to DatabaseSeeder

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(TaxAreasSeeder::class);
+        $this->call(VoyagerDatabaseSeeder::class);
     }
 }
