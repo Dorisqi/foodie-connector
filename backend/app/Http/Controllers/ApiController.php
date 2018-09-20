fdd6dd18684e170da49dc6b5648906798d228e49
Xiaonan Shen <sxn@shenxn.io>
2018-09-19 23:18:27 -0400

    [Add] Add base class ApiController

---
+++ backend/app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ApiController extends Controller
+{
+    /**
+     * Create response for a successful API call
+     *
+     * @param mixed $data [optional] Data to be returned to the client
+     * @return \Illuminate\Http\Response
+     */
+    protected function response($data = NULL)
+    {
+        $response_data = [
+            'succeed' => true,
+            'error_code' => 0,
+        ];
+        if (!is_null($data)) {
+            $response_data['data'] = $data;
+        }
+        return response()->json($response_data);
+    }
+
+}
