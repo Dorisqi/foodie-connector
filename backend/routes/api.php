459395c8d872ed8c1874adc94b3249077927ec1d
Xiaonan Shen <sxn@shenxn.io>
2018-09-24 18:39:49 -0400

    [Update] Update API url prefix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,9 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::prefix('user')->group(function () {
-    Route::post('register', 'User\RegisterController@register');
-    Route::post('login', 'User\LoginController@login');
-    Route::post('reset-email', 'User\ForgotPasswordController@sendResetLinkEmail');
+Route::prefix('v1')->group(function() {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', 'Auth\RegisterController@register');
+        Route::post('login', 'Auth\LoginController@login');
+        Route::post('reset-password-email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    });
 });
