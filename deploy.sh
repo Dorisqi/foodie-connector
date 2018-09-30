f5685b425dc328f91480dd9e1b7c9f13cf4d7b96
Xiaonan Shen <sxn@shenxn.io>
2018-09-29 20:04:11 -0400

    [Add] Add deploy step into app release

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-ssh -p 5749 sxn@foodie-connector.centralus.cloudapp.azure.com \
+ssh -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} \
     "sudo docker login --username ${DOCKER_HUB_USERNAME} --password ${DOCKER_HUB_PASSWORD} && \
-    sudo docker pull ${IMAGE_NAME}:${IMAGE_TAG} && \
-    sudo docker run -d --restart unless-stopped --network=webproxy --name=app-${IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER} -v /docker/app/${IMAGE_TAG}/storage/app:/var/www/storage/app ${IMAGE_NAME}:${IMAGE_TAG} && \
+    sudo docker pull ${IMAGE_NAME}:${[TYPE]_IMAGE_TAG} && \
+    sudo docker run -d --restart unless-stopped --network=webproxy --name=app-${[TYPE]_IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER} -v /docker/app/${[TYPE]_IMAGE_TAG}/storage/app:/var/www/storage/app ${IMAGE_NAME}:${[TYPE]_IMAGE_TAG} && \
     cd /docker/nginx/sites-available && \
     sudo mv app_upstream app_upstream.old && \
-    echo 'proxy_pass http://app-${IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER};' | sudo tee app_upstream"
+    sleep 10 && \
+    echo 'proxy_pass http://app-${[TYPE]_IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER};' | sudo tee app_upstream && \
+    sudo docker exec web nginx -s reload"
