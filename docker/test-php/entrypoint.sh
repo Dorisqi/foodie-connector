a26dc24f16712b83cf0dacddb42e84065d0c3d80
Xiaonan Shen <sxn@shenxn.io>
2018-10-01 11:42:33 -0400

    [Update] Fix typo in test-php entrypoint

--- docker/test-php/entrypoint.sh
+++ docker/test-php/entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 /usr/bin/redis-server --daemonize yes
-nohup /usr/local/stripe-mock/stripe-mock > /usr/local/strip-mock/log &
+nohup /usr/local/stripe-mock/stripe-mock > /usr/local/stripe-mock/log &
