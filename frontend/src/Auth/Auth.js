36d03a7e114fabed54f756f4627fc0272a010de3
Zequan Wu <wu1127@purdue.edu>
2018-10-03 12:45:09 -0400

    [Add] Auth util class

---
+++ frontend/src/Auth/Auth.js
@@ -0,0 +1,23 @@
+export default class Auth {
+  static authenticateUser(authentication, email) {
+    localStorage.setItem('authentication', authentication);
+    localStorage.setItem('email', email);
+  }
+
+  static isUserAuthenticated() {
+    return localStorage.getItem('authentication') !== null;
+  }
+
+  static deauthenticateUser() {
+    localStorage.removeItem('authentication');
+    localStorage.removeItem('email');
+  }
+
+  static getToken() {
+    return localStorage.getItem('authentication');
+  }
+
+  static getEmail() {
+    return localStorage.getItem('email');
+  }
+}
