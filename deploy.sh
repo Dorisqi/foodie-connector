70db09a421126c2130fdf084b0057e18620acfee
Xiaonan Shen <sxn@shenxn.io>
2018-10-10 15:43:59 -0400

    [Update] Improve deployment

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ ssh -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} \
     sudo docker pull ${IMAGE_NAME}:${[TYPE]_IMAGE_TAG} && \
     sudo docker run -d --restart unless-stopped --network=webproxy --name=app-${[TYPE]_IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER} -v /docker/app/${[TYPE]_IMAGE_TAG}/storage/app:/var/www/storage/app ${IMAGE_NAME}:${[TYPE]_IMAGE_TAG} && \
     cd /docker/nginx/sites-available && \
-    sudo mv app_upstream app_upstream.old && \
+    sudo mv app_upstream_${[TYPE]_IMAGE_TAG} app_upstream_${[TYPE]_IMAGE_TAG}.old && \
     sleep 10 && \
-    echo 'proxy_pass http://app-${[TYPE]_IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER};' | sudo tee app_upstream && \
+    echo 'proxy_pass http://app-${[TYPE]_IMAGE_TAG}-${BITBUCKET_BUILD_NUMBER};' | sudo tee app_upstream_${[TYPE]_IMAGE_TAG} && \
     sudo docker exec web nginx -s reload"
 
 BUILD_ID=${BITBUCKET_BRANCH}_${BITBUCKET_COMMIT}_${BITBUCKET_BUILD_NUMBER}
