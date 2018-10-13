841bfb0cf580b25b9f63d4f636becaa4d4908bc3
Xiaonan Shen <sxn@shenxn.io>
2018-10-13 17:57:03 -0400

    [Update] Fix typo in entrypoint

--- docker/deploy-base/entrypoint.sh
+++ docker/deploy-base/entrypoint.sh
@@ -6,7 +6,7 @@ cd /app/backend
 composer install --optimize-autoloader --no-dev
 php artisan config:cache
 php artisan route:cache
-php artisan storage --link
+php artisan storage:link
 php artisan migrate
 php artisan db:seed
 php artisan db:seed --class=VoyagerDatabaseSeeder
