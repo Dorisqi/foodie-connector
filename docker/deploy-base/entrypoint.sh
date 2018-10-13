1db22782ad07614dbdb498538f6ed67a47eb9707
Xiaonan Shen <sxn@shenxn.io>
2018-10-13 18:32:13 -0400

    [Update] Force migrate and seed in entrypoint

--- docker/deploy-base/entrypoint.sh
+++ docker/deploy-base/entrypoint.sh
@@ -7,9 +7,9 @@ composer install --optimize-autoloader --no-dev
 php artisan config:cache
 php artisan route:cache
 php artisan storage:link
-php artisan migrate
-php artisan db:seed
-php artisan db:seed --class=VoyagerDatabaseSeeder
+php artisan migrate --force
+php artisan db:seed --force
+php artisan db:seed --force --class=VoyagerDatabaseSeeder
 
 service php7.2-fpm start
 nginx -g 'daemon off;'
