9f36304b02f41d818ad8d603072c23c36faf3071
Zequan Wu <wzq9742@163.com>
2018-10-20 22:57:19 -0400

    add rest filters

--- frontend/src/pages/RestaurantListPage/RestaurantListPage.js
+++ frontend/src/pages/RestaurantListPage/RestaurantListPage.js
@@ -25,16 +25,25 @@ class RestaurantListPage extends React.Component {
   constructor() {
     super();
     this.state = {
+      address: '',
       place_id: '',
       tags: [],
+      tag_ids: '',
       originalList: [],
       changedList: [],
       nameMatchList: [],
+      filter_distance: '',
+      filter_estimated_delivery_time: '',
+      filter_delivery_fee: '',
+      filter_order_minimum: '',
+      filter_rating: '',
     };
     this.handleFilterChange = this.handleFilterChange.bind(this);
     this.handleSortChange = this.handleSortChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleOtherFilter = this.handleOtherFilter.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
   }
 
   componentWillMount() {
@@ -51,9 +60,8 @@ class RestaurantListPage extends React.Component {
         })
         .then(res2 => {
           if (res2.data) {
-            console.log(res2.data);
-            console.log(place_id);
             this.setState({
+              address: DEFAULT_ADDRESS,
               place_id: place_id,
               tags: res2.data.categories.slice(),
               originalList: res2.data.restaurants.slice(),
@@ -81,21 +89,55 @@ class RestaurantListPage extends React.Component {
       nameMatchList: changedList.filter(item => item.name.toLowerCase().indexOf(name.toLowerCase()) != -1)
     })
   }
+  getParams() {
+    const { place_id,
+            tag_ids,
+            filter_distance,
+            filter_estimated_delivery_time,
+            filter_delivery_fee,
+            filter_order_minimum,
+            filter_rating,
+          } = this.state;
 
-  handleSubmit(place_id, tag_ids='') {
     const params = {
       place_id: place_id,
     }
     if (tag_ids && tag_ids.length > 0) {
       params['filter_categories'] = tag_ids;
     }
+    params['filter_distance'] = filter_distance;
+    params['filter_estimated_delivery_time'] = filter_estimated_delivery_time;
+    params['filter_delivery_fee'] = filter_delivery_fee;
+    params['filter_order_minimum'] = filter_order_minimum;
+    params['filter_rating'] = filter_rating;
+
+    return params;
+  }
+
+  handleSubmit(address='') {
+    if (address === '') {
+      address = this.state.address;
+    }
+    Geocode.fromAddress(address)
+    .then(
+      res => {
+        this.getRestaurants(res.results[0].place_id);
+      }
+    , err => {
+        console.log(err)
+      }
+    )
+    //event.preventDefault();
+  }
+  getRestaurants(place_id) {
+    const params = this.getParams();
     axios.get(apiList.restaurants, {
       params: params
     })
     .then(res => {
       if (res.data) {
-        console.log(res.data);
         this.setState({
+          place_id: place_id,
           originalList: res.data.restaurants.slice(),
           changedList: res.data.restaurants.slice(),
           nameMatchList: res.data.restaurants.slice(),
@@ -108,6 +150,16 @@ class RestaurantListPage extends React.Component {
     .catch(err => {
       console.log(err);
     })
+  }
+
+  handleOtherFilter(filters) {
+    this.setState({
+        filter_distance: filters.distanceLo+'_'+filters.distanceHi,
+        filter_estimated_delivery_time: filters.deliveryTimeLo+'_'+filters.deliveryTimeHi,
+        filter_delivery_fee: filters.deliveryFeeLo+'_'+filters.deliveryFeeHi,
+        filter_order_minimum: filters.orderMinLo+'_'+filters.orderMinHi,
+        filter_rating: filters.ratingLo+'_'+filters.ratingHi,
+      }, () => this.handleSubmit());
   }
 
   handleFilterChange(selectedTags) {
@@ -131,30 +183,49 @@ class RestaurantListPage extends React.Component {
     if (tag_ids !== '') {
       tag_ids = tag_ids.slice(1, tag_ids.length);
     }
-    this.handleSubmit(place_id, tag_ids);
+    this.setState({
+      tag_ids: tag_ids
+    })
   }
 
   handleSortChange(sortOrder) {
-    const { changedList } = this.state;
-    this.setState({
-      changedList: changedList.sort((o1, o2) => sortOrder(o1, o2)),
-    });
+    const { nameMatchList } = this.state;
+    if (sortOrder === 'default') {
+      this.setState({
+        changedList: nameMatchList
+      })
+    }
+    else if (sortOrder === 'rating') {
+      this.setState({
+        changedList: nameMatchList.concat().sort((o1, o2) => o2.rating - o1.rating),
+      })
+    }
+    else {
+      this.setState({
+        changedList: nameMatchList.concat().sort((o1, o2) => o1[sortOrder] - o2[sortOrder]),
+      })
+    }
+  }
+
+  handleAddressChange(address) {
+    this.setState({ address: address });
   }
 
   render() {
-    const { nameMatchList, tags } = this.state;
+    const { changedList, tags } = this.state;
     const { classes } = this.props;
     return (
         <div className={classes.paper}>
-        <AddressSearchBar onSubmit={this.handleSubmit}></AddressSearchBar>
+        <AddressSearchBar handleAddressChange={this.handleAddressChange}></AddressSearchBar>
           <RestaurantFilter
             onFilterChange={this.handleFilterChange}
             onSortChange={this.handleSortChange}
             handleNameChange={this.handleNameChange}
             tags={tags}
+            handleOtherFilter={this.handleOtherFilter}
           />
 
-          <RestaurantList restaurantList={nameMatchList} />
+          <RestaurantList restaurantList={changedList} />
         </div>
 
     );
