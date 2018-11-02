84984507e9e3311c280549f88c98f198986ef6f0
shuqi1222 <ma460@purdue.edu>
2018-11-02 18:03:05 -0400

    reset pw finished

--- frontend/src/apiList.js
+++ frontend/src/apiList.js
@@ -13,12 +13,14 @@ const apiList = {
   profilePassword: '/api/v1/profile/password',
   addresses: '/api/v1/addresses',
   addressDetail: '/api/v1/addresses/',
+  deleteaddress:(id)=>('/api/v1/addresses/'+id),
+  updateaddress: (id)=>('/api/v1/addresses/'+id),
   card: '/api/v1/cards',
   cardDetail: '/api/v1/cards/',
   restaurants: '/api/v1/restaurants',
   restaurant: (id) => ('/api/v1/restaurants/'+id),
   createorder:'/api/v1/orders',
-  cart: '/api/v1/cart',
+
 }
 
 export default apiList;
--- frontend/src/components/UserAddress/EditAddress.js
+++ frontend/src/components/UserAddress/EditAddress.js
@@ -162,9 +162,7 @@ constructor(props){
   handleConfirm(modal,event){
     const { id,name, phone, line_1, line_2, city, state, zip_code, place_id, is_default } = this.state;
     const { handleAddAddress } = this.props;
-    const x = [];
-    x[modal] = true;
-    this.setState(x);
+
     alert(id+name+phone+line_1+line_2+city+state+zip_code);
 
     axios.put(`/api/v1/addresses/${id}`, {
@@ -184,7 +182,11 @@ constructor(props){
     alert("Address changed successfully!");
     this.setState({id:id,phone:phone,line_1:line_1,line_2:line_2
                     ,city:city,state:state,zip_code:zip_code});
+                    const x = [];
+                    x[modal] = false;
+                    this.setState(x);
     }).catch(err => {
+      
       console.log(err);
       const { response } = err;
       if (response && response.status === 401) {
@@ -198,6 +200,7 @@ constructor(props){
       }
 
     })
+
 
   }
 
