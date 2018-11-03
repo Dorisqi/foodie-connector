c059e2fd2a51a58f38423123a128c19686d6d938
Xiaonan Shen <sxn@shenxn.io>
2018-11-03 18:56:54 -0400

    [Update] Use react-app rules for eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "no-param-reassign": 0
-    },
-    "env": {
-        "jest": true,
-        "browser": true,
-    },
-    "settings": {
-      "import/resolver": {
-        "node": {
-          "paths": ["src/material-kit", "src/"]
-        }
-      }
-    }
-
+    "extends": "react-app",
 };
